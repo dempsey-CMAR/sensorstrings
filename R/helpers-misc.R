c8026f28d94f78792a4e16f75fee40b6b03aa270
Danielle Dempsey <ddempsey@perennia.ca>
2022-08-23 16:01:59 -0300

    initial commit

---
+++ R/helpers-misc.R
@@ -0,0 +1,19 @@
+#' @title Extracts the extension of a file name
+#'
+#' @details Extracts the file extension from a character string using //. as the
+#'   separator.
+#'
+#' @param file_name Character string of a file name or path. Must only include
+#'   one ".", which is used as the separator.
+#'
+#' @importFrom tidyr separate
+
+extract_file_extension <- function(file_name){
+
+  extension <- file_name %>%
+    data.frame() %>%
+    separate(col = 1, into = c(NA, "EXT"), sep = "\\.")
+
+  extension$EXT
+}
+
---
+++ R/ss_read_log.R
@@ -0,0 +1,223 @@
+#' @title Extract information from deployment log
+#' @details The log must be saved in a folder called Log in .csv, .xlsx or .xls
+#'   format, and must include the following columns:
+#'
+#'   \code{Deployment_Waterbody}: waterbody where string was deployed
+#'
+#'   \code{Location_Description}: the station name
+#'
+#'   \code{Lease#}: If located on an aquaculture site, the lease number (NA
+#'   otherwise)
+#'
+#'   \code{Deployment}: The deployment date, in the order "Ymd"
+#'
+#'   \code{Retrieval}: The retrieval date, in the order "Ymd"
+#'
+#'   \code{Logger_Latitude}: The latitude at which the string was deployed
+#'
+#'   \code{Logger_Longitude} The longitude at which the string was deployed
+#'
+#'   \code{Logger_Model} The type of sensor; see below for options
+#'
+#'   \code{Serial#} The sensor serial number
+#'
+#'   \code{Sensor_Depth}: Depth at which the sensor was deployed
+#'
+#'   All other columns will be ignored.
+#'
+#'   Entries in the \code{Logger_Model} column can be "HOBO Pro V2", "HOBO DO",
+#'   "TidbiT MX2303", "TidbiT MX2203"  "aquaMeasure DOT", "aquaMeasure SAL",
+#'   "aquaMeasure SST", or "VR2AR". (Some mis-spellings are accepted: "HOBO pro
+#'   V2", "HOBO_Pro_V2", "aquameasure DOT", "aquameasure SAL", "aquameasure
+#'   SST")
+#'
+#'   A Warning message is printed to the console when the function does not
+#'   recognize a sensor in the log.
+#'
+#'   A message is printed to the console when Hobo, aquaMeasure, or Vemco
+#'   sensors are not found in the log.
+#'
+#'   A message will be printed to the console if there is more than one unique
+#'   entry in \code{Deployment_Waterbody}, \code{Location_Description},
+#'   \code{Deployment}, \code{Retrieval}, \code{Logger_Latitude}, or
+#'   \code{Logger_Longitude}.
+#'
+#'   If there is more than one eligible file (csv, .xlsx or .xls) in the Log
+#'   folder, the function will stop with an error.
+#'
+#' @param path File path to the Log folder.
+#'
+#' @return Returns a list with 5 elements. \code{deployment.dates} is a
+#'   dataframe with two columns: \code{start.date} (the date of deployment) and
+#'   \code{end.date} (date of retrieval). \code{area.info} is a dataframe with
+#'   five columns: \code{waterbody}, \code{latitude}, \code{longitude},
+#'   \code{station}, and \code{lease}. \code{HOBO}, \code{aM}, and \code{vemco}
+#'   are each a dataframe with two columns: \code{SENSOR} (serial number) and
+#'   the
+#'
+#'   corresponding \code{DEPTH} (depth of deployment in m).
+#' @family compile
+#' @author Danielle Dempsey
+#'
+#' @importFrom glue glue
+#' @importFrom tidyr separate
+#' @importFrom stringr str_replace str_detect
+#' @importFrom readxl read_excel
+#' @importFrom data.table fread
+#' @importFrom lubridate ymd
+#' @importFrom dplyr %>% contains filter mutate select
+#'
+#' @export
+
+ss_read_log <- function(path){
+
+# Read in log -----------------------------------------------------------
+
+  path <- glue("{path}/Log")
+
+  dat_files <- list.files(path, all.files = FALSE, pattern = "*xlsx|*xls|*csv")
+
+  file_type <- extract_file_extension(dat_files)
+
+  # remove files that start with "~"
+  if(any(substring(dat_files, 1, 1) == "~")) {
+
+    message(paste("Note:", sum((substring(dat_files, 1, 1)== "~")),
+                "files on the path begin with ~ and were not imported.", sep = " "))
+    dat_files <- dat_files[-which(substring(dat_files, 1, 1)== "~")]
+
+  }
+
+  if (length(dat_files) > 1) stop("More than one file found in the Log folder")
+
+  if (file_type == "xls" |  file_type == "xlsx") {
+    log <- read_excel(paste(path, dat_files, sep = "/"))
+  }
+
+  if (file_type == "csv") {
+    log <- fread(paste(path, dat_files, sep = "/"), data.table = FALSE)
+  }
+
+
+  # deployment dates ------------------------------------------------------------
+
+  # deployment dates
+  start  <- unique(log$Deployment)
+  end <- unique(log$Retrieval)
+
+  # message if there is more than one Deployment or Retrieval date
+  if(length(start) > 1 | length(end) > 1) {
+    message("Multiple Deployment or Retrieval dates in log")
+  }
+
+  # Stop with ERROR if the dates are not in the proper format
+  if(is.na(suppressWarnings(lubridate::ymd(log$Deployment[1]))) |
+     is.na(suppressWarnings(lubridate::ymd(log$Retrieval[1])))) {
+    stop("Deployment and Retrieval dates must be in the order year, month, day")
+  }
+
+  # deployment info to export
+  deployment_dates <- data.frame(
+    start_date = lubridate::ymd(log$Deployment[1]),
+    end_date = lubridate::ymd(log$Retrieval[1])
+  )
+
+# area info ---------------------------------------------------------------
+
+  wb <- unique(log$Deployment_Waterbody)
+  lat <- unique(log$Logger_Latitude)
+  long <- unique(log$Logger_Longitude)
+  station <- unique(log$Location_Description)
+  lease <- unique(log$`Lease#`)
+
+  if(length(wb) > 1) message("Multiple waterbodies in log")
+  if(length(lat) > 1) message("Multiple latitudes recorded in log")
+  if(length(long) > 1) message("Multiple longitudes recorded in log")
+  if(length(station) > 1) message("Multiple location descriptions recorded in log")
+  if(length(lease) > 1) message("Multiple leases recorded in log")
+
+  if(long > 0) stop("Longitude must be a negative value")
+
+  area_info <- data.frame(
+    waterbody = log$Deployment_Waterbody[1],
+    latitude = log$Logger_Latitude[1],
+    longitude = log$Logger_Longitude[1],
+    station  = log$Location_Description[1],
+    lease = log$`Lease#`[1]
+  )
+
+
+# depth -------------------------------------------------------------------
+
+  depth <- log %>%
+    select(Logger_Model, `Serial#`, Sensor_Depth) %>%
+    mutate(numeric_depth = suppressWarnings(as.numeric(Sensor_Depth)))
+
+  if (any(is.na((depth$numeric_depth)))) {
+    depth_char <- depth %>%
+      filter(is.na(numeric_depth)) %>%
+      select(-numeric_depth) %>%
+      mutate(depth_print = paste(Logger_Model, `Serial#`, Sensor_Depth))
+
+    warning(
+      paste0("Depth can't be converted to numeric: ", depth_char$depth_print, "\n")
+    )
+  }
+
+
+# serial number table -----------------------------------------------------
+
+  sn_table <- log %>%
+    select(sensor = Logger_Model, serial = `Serial#`, depth = Sensor_Depth)
+
+  sensors <- sn_table %>%
+    mutate(
+      sensor = str_replace(sensor, "_", " "),
+      sensor = tolower(sensor),
+      detect_hobo = str_detect(sensor, "hobo"),
+      detect_tidbit = str_detect(sensor, "tidbit"),
+      detect_am = str_detect(sensor, "aquameasure"),
+      detect_vemco = str_detect(sensor, "vr2ar")
+    )
+
+  # warning if there are any sensors in the log that are NOT recognized by package
+  n_sensors <- sensors %>%
+    select(contains("detect")) %>%
+    apply(1, sum)
+
+   # select(contains("detect")) %>%
+   # mutate(total = sum(c_across(contains("detect"))))
+    #mutate(total = rowSums(.))
+
+  if (any(n_sensors == 0)) {
+    extra_sensor <- sensors[which(n_sensors == 0), "sensor"]
+
+    warning(
+      glue("{extra_sensor} found in the Logger_Model column of the log.
+            This sensor is not recognized by the sensorstrings package")
+    )
+  }
+
+# message if any expected sensors not found
+  if (nrow(filter(sensors, detect_hobo == TRUE)) == 0) {
+    message("No Hobo sensors found in log")
+  }
+
+
+  if (nrow(filter(sensors, detect_am == TRUE)) == 0){
+    message("No aquaMeasure sensors found in log")
+  }
+
+  if(nrow(filter(sensors, detect_vemco == TRUE)) == 0){
+    message("No VR2AR sensors found in log")
+  }
+
+
+# return list of deployment info -------------------------------------------
+  list(
+    deployment_dates = deployment_dates,
+       area_info = area_info,
+       sn_table = sn_table
+    )
+
+}
