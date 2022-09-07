ed08c3b0b9d4f692e58b0e9237618bce55b66add
Danielle Dempsey <ddempsey@perennia.ca>
2022-09-07 14:52:45 -0300

    remove more hobo tests

--- tests/testthat/test-helpers-compile.R
+++ tests/testthat/test-helpers-compile.R
@@ -89,24 +89,24 @@ test_that("convert_timestamp_to_datetime() works", {
 
 # extract_hobo_sn() -------------------------------------------------------
 
-test_that("extract_hobo_sn() works", {
-  expect_equal(extract_hobo_sn(hobo_colnames), 10755220)
-  expect_error(extract_hobo_sn(hobo_colnames_error))
-})
+# test_that("extract_hobo_sn() works", {
+#   expect_equal(extract_hobo_sn(hobo_colnames), 10755220)
+#   expect_error(extract_hobo_sn(hobo_colnames_error))
+# })
 
 
 # extract_hobo_units() ----------------------------------------------------
-
-test_that("extract_hobo_units() returns correct units", {
-  expect_equal(hobo_units$units, c("utc", "degree_C", "mg_per_L"))
-})
+#
+# test_that("extract_hobo_units() returns correct units", {
+#   expect_equal(hobo_units$units, c("utc", "degree_C", "mg_per_L"))
+# })
 
 
 # make_column_names() -------------------------------------------------------
 
-test_that("make_column_names() returns correct names", {
-  expect_equal(
-    new_hobo_colnames$col_name,
-    c("timestamp_utc", "dissolved_oxygen_mg_per_L", "temperature_degree_C")
-  )
-})
+# test_that("make_column_names() returns correct names", {
+#   expect_equal(
+#     new_hobo_colnames$col_name,
+#     c("timestamp_utc", "dissolved_oxygen_mg_per_L", "temperature_degree_C")
+#   )
+# })
