aa32adff31228eb5b643cb7ab2391ed0262bac67
Danielle Dempsey <ddempsey@perennia.ca>
2022-09-07 15:18:44 -0300

    I think changing the folder name fixed the error

--- tests/testthat/test-ss_hobo.R
+++ tests/testthat/test-ss_hobo.R
@@ -26,43 +26,43 @@ test_that("ss_read_hobo_data() report error for excel files", {
 
 # ss_compile_hobo_data ----------------------------------------------------
 
-# test_that("ss_compile_hobo_data() returns correct classes", {
-#   expect_equal(class(hobo_trim$deployment_range), "character")
-#   expect_equal(class(hobo_trim$timestamp_utc), c("POSIXct", "POSIXt"))
-#   expect_equal(class(hobo_trim$sensor), "character")
-#   expect_equal(class(hobo_trim$sensor_depth_at_low_tide_m), "numeric")
-#   expect_equal(class(hobo_trim$temperature_degree_C), "numeric")
-#   expect_equal(class(hobo_trim$dissolved_oxygen_mg_per_L), "numeric")
-# })
-#
-#
-# test_that("ss_compile_hobo_data() reads in all observations", {
-#   expect_equal(nrow(hobo_all), 49)
-#   expect_equal(nrow(hobo_trim), 36)
-# })
-#
-# test_that("ss_compile_hobo_data() returns error if file name does not match entries in sn_table",{
-#
-#   expect_error(
-#     ss_compile_hobo_data(
-#       path,
-#       sn_table = data.frame(
-#         sensor = c("HOBO", "HOBO"),
-#         serial = c(1234, 5678),
-#         depth = c(1, 2)
-#       ),
-#       deployment_dates = deployment_dates
-#     )
-#   )
-# })
-#
-# test_that("ss_compile_hobo_data() returns Error if trimming removes all rows", {
-#   expect_error(
-#     ss_compile_hobo_data(
-#       path = path,
-#       deployment_dates = data.frame(START = "2020-05-30", END = "2020-10-19"),
-#       sn_table = sn_hobo
-#     )
-#   )
-# })
+test_that("ss_compile_hobo_data() returns correct classes", {
+  expect_equal(class(hobo_trim$deployment_range), "character")
+  expect_equal(class(hobo_trim$timestamp_utc), c("POSIXct", "POSIXt"))
+  expect_equal(class(hobo_trim$sensor), "character")
+  expect_equal(class(hobo_trim$sensor_depth_at_low_tide_m), "numeric")
+  expect_equal(class(hobo_trim$temperature_degree_C), "numeric")
+  expect_equal(class(hobo_trim$dissolved_oxygen_mg_per_L), "numeric")
+})
+
+
+test_that("ss_compile_hobo_data() reads in all observations", {
+  expect_equal(nrow(hobo_all), 49)
+  expect_equal(nrow(hobo_trim), 36)
+})
+
+test_that("ss_compile_hobo_data() returns error if file name does not match entries in sn_table",{
+
+  expect_error(
+    ss_compile_hobo_data(
+      path,
+      sn_table = data.frame(
+        sensor = c("HOBO", "HOBO"),
+        serial = c(1234, 5678),
+        depth = c(1, 2)
+      ),
+      deployment_dates = deployment_dates
+    )
+  )
+})
+
+test_that("ss_compile_hobo_data() returns Error if trimming removes all rows", {
+  expect_error(
+    ss_compile_hobo_data(
+      path = path,
+      deployment_dates = data.frame(START = "2020-05-30", END = "2020-10-19"),
+      sn_table = sn_hobo
+    )
+  )
+})
 
