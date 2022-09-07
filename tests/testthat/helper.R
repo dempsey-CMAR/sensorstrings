40ee08a6664b073168732a34d26fa9787ffd2ac8
Danielle Dempsey <ddempsey@perennia.ca>
2022-09-07 14:35:29 -0300

    found more hobo lines to comment

--- tests/testthat/helper.R
+++ tests/testthat/helper.R
@@ -133,28 +133,28 @@ ts_error <- data.frame(timestamp_ = "08-22-2019 12:55")
 
 # extract_hobo_sn ---------------------------------------------------------
 
-hobo_colnames <- c(
-  "Date Time, GMT+00:00",
-  "Temp, °C (LGR S/N: 10755220, SEN S/N: 10755220)"
-)
-
-hobo_colnames_error <- c(
-  "Date Time, GMT+00:00",
-  "Temp, °C (LGR S/N: 10755220, SEN S/N: 123456)"
-)
+# hobo_colnames <- c(
+#   "Date Time, GMT+00:00",
+#   "Temp, °C (LGR S/N: 10755220, SEN S/N: 10755220)"
+# )
+#
+# hobo_colnames_error <- c(
+#   "Date Time, GMT+00:00",
+#   "Temp, °C (LGR S/N: 10755220, SEN S/N: 123456)"
+# )
 
 
 # extract_hobo_units ---------------------------------------------------
 
-hobo_units <- ss_read_hobo_data(
-  path = system.file("testdata/hobo", package = "sensorstrings"),
-  file_name = "20827226.csv"
-) %>%
-  extract_hobo_units()
+# hobo_units <- ss_read_hobo_data(
+#   path = system.file("testdata/hobo", package = "sensorstrings"),
+#   file_name = "20827226.csv"
+# ) %>%
+#   extract_hobo_units()
 
 # make_column_names -------------------------------------------------------
 
-new_hobo_colnames <- make_column_names(hobo_units)
+# new_hobo_colnames <- make_column_names(hobo_units)
 
 
 # ss_pivot ----------------------------------------------------------------
