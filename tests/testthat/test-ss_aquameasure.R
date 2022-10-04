9a347d54a7d3ee42c3dfcd4610b164408d237b1e
Danielle Dempsey <ddempsey@perennia.ca>
2022-10-04 15:00:49 -0300

    changes commensurate with package updates

--- tests/testthat/test-ss_aquameasure.R
+++ tests/testthat/test-ss_aquameasure.R
@@ -6,7 +6,7 @@ test_that("ss_read_aquameasure_data() reads in correct classes", {
   expect_equal(class(am1$`Time Corrected(seconds)`), "integer")
   expect_equal(class(am1$Sensor), "character")
   expect_equal(class(am1$`Record Type`), "character")
-  expect_equal(class(am1$`Dissolved Oxygen`), "numeric")
+  expect_equal(class(am1$`Dissolved Oxygen`), "character") # because has entry "ERR"
   expect_equal(class(am1$Temperature), "numeric")
   expect_equal(class(am1$`Device Tilt`), "numeric")
   expect_equal(class(am1$`Battery Voltage`), "numeric")
