3071d68600aaf07e29a4b9c29a7f15a86293aaa7
Danielle Dempsey <ddempsey@perennia.ca>
2023-03-08 11:04:38 -0400

    initial commit

---
+++ R/ss_open_trimdates_app.R
@@ -0,0 +1,92 @@
+#
+# library(shiny)
+# library(ggplot2)
+# library(lubridate)
+# library(plotly)
+# library(sensorstrings)
+# library(data.table)
+
+#' Open interactive plot
+#'
+#' @param dat Data frame of sensor string data in wide format, as exported from
+#'   \code{ss_compile_deployment_data()}.
+#'
+#' @param filter_to Shortcut for specifying where to filter \code{dat} before
+#'   plotting. Options are "start", "end", or "custom".
+#'
+#' @param period Character string that can be convert to a \code{lubridate}
+#'   period.
+#'
+#' @param custom_start Only required if \code{filter_to = "custom"}. POSIXct
+#'   object indicating where the filtered data will begin.
+#'
+#' @param custom_end Only required if \code{filter_to = "custom"}. POSIXct
+#'   object indicating where the filtered data will end.
+#'
+#' @param point_size Size of points in the plot.
+#'
+#' @return Opens a shiny app displaying an interactive plot of variables in
+#'   \code{dat}.
+#'
+#' @importFrom shiny fluidPage  renderPrint shinyApp verbatimTextOutput
+#' @importFrom plotly ggplotly event_data plotlyOutput renderPlotly
+#' @importFrom lubridate as_datetime
+#'
+#' @export
+
+
+ss_open_trimdates_app <- function(
+    dat,
+    filter_to = c("start", "end", "custom"),
+    period = "2 days",
+    custom_start = NULL,
+    custom_end = NULL,
+    point_size = 2
+) {
+
+  # Define UI for application that draws a histogram
+  ui <- fluidPage(
+
+    plotlyOutput("vars_plot", height="600px"),
+    verbatimTextOutput("info")
+  )
+
+  # Define server logic required to draw a histogram
+  server <- function(input, output) {
+
+    output$vars_plot <- renderPlotly({
+
+      dat  <- dat %>%
+        filter_dat_to_plot(
+          filter_to = filter_to,
+          period = period,
+          custom_start = custom_start,
+          custom_end = custom_start
+        )
+
+      p <- ss_ggplot_variables(dat) + geom_point(size = point_size)
+
+      ggplotly(p, source = "plot1", tooltip = "text")
+
+    })
+
+    output$info <- renderPrint({
+
+      point_info <- event_data("plotly_click", source = "plot1")
+
+      if (is.null(point_info)) {
+
+        "Click events appear here (double-click chart to clear)"
+
+      } else {
+
+        as_datetime(point_info$x)
+
+      }
+    })
+  }
+
+  # Run the application
+  shinyApp(ui = ui, server = server)
+
+}
