f0ee83be45b7eb2f1e150a570fac9dffdb0b3561
Danielle Dempsey <ddempsey@perennia.ca>
2024-10-04 15:08:30 -0300

    initial commit

---
+++ R/ss_create_deployment_log.R
@@ -0,0 +1,135 @@
+#' @title Writes a deployment log from the metadata tracking sheet
+#'
+#' @details Imports the NSDFA tracking sheet, filters for the station and date of
+#'  interest, re-formats into the deployment log format, and exports to the Log
+#'  folder.
+#'
+#' @param path_metadata Path to the metadata tracking sheet (including
+#'  the file name and extension). This must be a .xlsx file.
+#'
+#' @param sheet Name of the tab with the deployment information.
+#'
+#' @param path_export Path to the station deployment folder.
+#'
+#' @param station Station name.
+#'
+#' @param deployment_date Date of deployment as a character string in the format
+#'  "YYYY-mm-dd"
+#'
+#' @return Returns deployment log in .csv format.
+#'
+#' @importFrom readxl read_excel
+#' @importFrom lubridate as_date
+#' @importFrom stringr str_to_title
+#' @importFrom dplyr %>% filter mutate transmute
+#' @importFrom utils write.csv
+#'
+#' @export
+
+create_log_from_metadata <- function(
+    path_metadata = NULL,
+    sheet = "tracker",
+    path_export,
+    station,
+    deployment_date
+){
+
+  if(is.null(path_metadata)) {
+    path_metadata <- file.path(
+      "R:/tracking_sheets/metadata_tracking/water_quality_deployment_tracking.xlsx"
+    )
+  }
+
+  dat_raw <- read_excel(
+    path_metadata,
+    sheet = sheet,
+    na = ""
+  )
+
+  if(!(station %in% dat_raw$station)) {
+    stop(paste0(station, " not found in metatdata tracking sheet"))
+  }
+
+ log <- dat_raw %>%
+    filter(
+      station == !!station,
+      deployment_date == !!as_date(deployment_date)
+    ) %>%
+    transmute(
+      Deployment_Waterbody = waterbody,
+      Location_Description = station,
+      `Lease#` = lease,
+      Status = status,
+
+      Deployment = deployment_date,
+      Retrieval =  retrieval_date,
+      Duration = difftime(retrieval_date, deployment_date),
+      Logger_Latitude = deployment_latitude,
+      Logger_Longitude = deployment_longitude,
+      Logger_Model = sensor_type,
+      `Serial#` = sensor_serial_number,
+      Sensor_Depth = sensor_depth_m,
+
+      Sounding = sounding_m,
+      Datum = NA,
+      Mount_type = NA,
+
+      #`Acoustic_Release?` = ifelse(Recv_Method == "Acoustic_Release", "Y", NA_character_),
+      #`Surface_Buoy?` = ifelse(Recv_Method == "Surface Buoy", "Y", NA_character_),
+
+      Deployment_Attendant = deployment_attendant,
+      Retrieval_Attendant = retrieval_attendant,
+
+      Comments = notes,
+
+     # `Deployment Waypoint` = NA,
+      #`Retrieval Waypoint` = NA,
+
+      `Retrieval Latitude`	= retrieval_latitude,
+      `Retrieval Longitude` =	retrieval_longitude,
+
+      #`Sensor Voltage deployed` = Depl_Voltage,
+      #`Sensor Voltage retrieved` = Recv_Voltage,
+
+      #`Vessel sounder offset + transponder depth` = NA,
+      #`verified measurement (below origin, first sensor under float)` = NA,
+      #`tide correction` = NA,
+      #`Rising or Falling` = NA,
+      #`height of VR2AR base off bottom` = NA,
+
+      #`time of deployment` = Depl_Time,
+      #`photos taken?` = NA,
+
+      #`Anchor type` = Anchor_Wgt,
+      #`Float type` = NA,
+      #`distance from top of float to origin (first sensor)` = NA
+    ) %>%
+    dplyr::mutate(
+      Location_Description = str_to_title(Location_Description),
+      `Serial#` = as.numeric(`Serial#`),
+      Sensor_Depth = as.numeric(Sensor_Depth),
+      Sounding = as.numeric(Sounding),
+      Deployment = format(as_date(Deployment), "%Y-%b-%d"),
+      Retrieval = format(as_date(Retrieval), "%Y-%b-%d"),
+    )
+
+  # if(suppressWarnings(is.numeric(as.numeric(LOG$Sensor_Depth)))){
+  #   LOG <- LOG %>% mutate(Sensor_Depth = as.numeric(Sensor_Depth))
+  # }
+
+  # Format & Export ---------------------------------------------------------
+ file_name <- paste(
+   station, format(as_date(deployment_date), "%Y-%m-%d"),
+   "Log.csv", sep = "_"
+ )
+
+ write.csv(log, file = paste(path_export, file_name, sep = "/"))
+
+  # LOG_OUT <- LOG %>%
+  #   format_deployment_log() %T>%
+  #   saveWorkbook(file = paste(path.export, file.name, sep = "/"))
+
+  message(file_name, " exported to ", path_export)
+}
+
+
