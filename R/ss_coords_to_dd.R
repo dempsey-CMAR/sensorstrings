93a50c016d1429549492bee03b00d3824c4486b8
Danielle Dempsey <ddempsey@perennia.ca>
2024-10-07 14:52:43 -0300

    re-write to work with across()

--- R/ss_coords_to_dd.R
+++ R/ss_coords_to_dd.R
@@ -12,6 +12,7 @@
 #'
 #' @importFrom dplyr across mutate select
 #' @importFrom tidyr separate
+#' @importFrom data.table :=
 #'
 #' @export
 #'
@@ -51,4 +52,74 @@ ss_coords_to_decimal_degrees <- function(dat) {
     ) %>%
     select(-c(lat_deg, lat_md, long_deg, long_md))
 
+}
+
+
+
+#coord_column <- "deployment_latitude_n_ddm"
+
+ss_coords_from_ddm_to_dd <- function(dat, coord_column) {
+
+  coord_column_lwr <- tolower(coord_column)
+
+  if (str_detect(coord_column_lwr, pattern = "lat|latitude")) {
+    coord <- "latitude"
+  } else if (str_detect(coord_column_lwr, pattern = "long|longitude")) {
+    coord <- "longitude"
+  } else {
+    stop("coord_column must include lat, latitude, long, or longitude")
+  }
+
+  if (str_detect(coord_column_lwr, pattern = "deployment|depl")) {
+    phase <- "deployment_"
+  } else if (str_detect(coord_column_lwr, pattern = "retrieval")) {
+    phase <- "retrieval_"
+  } else {
+    phase <- ""
+  }
+
+#  browser()
+
+  new_name <- paste0(phase, coord)
+
+  coord_column2 <- enquo(coord_column)
+
+
+  dat %>%
+    rename(degree_decimal_minutes = !! coord_column2) #%>%
+    #rename(degree_decimal_minutes = {{ coord_column }}) #%>%
+    # separate(
+    #   degree_decimal_minutes,
+    #   into = c("coord_deg", "coord_dm"), sep = " ", remove = FALSE
+    # ) %>%
+    # mutate(
+    #   across(contains("coord_"), ~ as.numeric(.x)),
+    #   decimal_degree = coord_deg + coord_dm / 60
+    # ) %>%
+    # select(-c(coord_deg, coord_dm)) %>%
+    # rename(
+    #   #new_name = decimal_degree,
+    #   #coord_column = degree_decimal_minutes
+    #    "{new_name}" := decimal_degree,
+    #    "{coord_column}" := degree_decimal_minutes
+    # )
+}
+
+
+
+
+ss_coords_from_ddm_to_dd2 <- function(coord_column) {
+
+  x_out <- coord_column %>%
+    data.frame(degree_decimal_minutes = .) %>%
+    separate(
+      degree_decimal_minutes,
+      into = c("coord_deg", "coord_dm"), sep = " ", remove = FALSE
+    ) %>%
+    mutate(
+      across(contains("coord_"), ~ as.numeric(.x)),
+      decimal_degree = coord_deg + coord_dm / 60
+    )
+
+  x_out$decimal_degree
 }
