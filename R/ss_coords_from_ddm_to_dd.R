8d339e82588e193165934b5038ff72db8e8be5a6
Danielle Dempsey <ddempsey@perennia.ca>
2024-10-23 11:12:04 -0300

    update documentation and clean up comments

--- R/ss_coords_from_ddm_to_dd.R
+++ R/ss_coords_from_ddm_to_dd.R
@@ -1,11 +1,22 @@
-#' Convert coordinates from degree-minute-decimal to decimal degree #'
+#' Convert coordinates from degree-minute-decimal to decimal degree
+#'
+#' To apply to multiple columns using \code{dplyr::across}:
+#'
+#' 1. Define variables to convert:
+#'
+#' coords_ddm <- c("deployment_latitude_n_ddm", "deployment_longitude_w_ddm",
+#' "retrieval_latitude_n_ddm", "retrieval_longitude_w_ddm")
+#'
+#' 2. Use the .names argument to specify new column names so that the original
+#' columns are not over-written:
+#'
+#' dat <- dat_raw %>% mutate( across(any_of(coords_ddm),
+#' ~ss_coords_from_ddm_to_dd(.x), .names = "{str_remove(.col, '_w_ddm|_n_ddm')}"
+#' ))
+#'
 #' @param coords_ddm Vector of coordinate values in degree decimal minutes (xx
 #'   xx.xxxx, e.g., 45 21.651 or 64 2.063). These will be converted to decimal
 #'   degrees.
-#'
-#@param negative_hemisphere Logical argument indicating whether to multiply
-#'   the output by -1 (e.g., for latitude in the Southern hemisphere and
-#'   longitude in the Western hemisphere).
 #'
 #' @return Returns a vector of coordinates in decimal-degree format.
 #'
@@ -13,14 +24,6 @@
 #' @importFrom tidyr separate
 #'
 #' @export
-
-# dat <- data.frame(
-#   deployment_latitude = c(NA, NA),
-#   deployment_longitude = c(NA, NA),
-#   deployment_latitude_n_ddm = c("45 39.613", "45 42.482"),
-#   deployment_longitude_w_ddm = c("61 54.245", "61 51.392")
-# )
-#, negative_hemisphere = FALSE
 
 ss_coords_from_ddm_to_dd <- function(coords_ddm) {
 
@@ -33,10 +36,6 @@ ss_coords_from_ddm_to_dd <- function(coords_ddm) {
     mutate(
       across(contains("coord_"), ~ as.numeric(.x)),
       decimal_degree = coord_deg + coord_dm / 60
-#
-#       decimal_degree = if_else(
-#         isTRUE(negative_hemisphere),
-#         -decimal_degree, decimal_degree)
     )
 
   coords_out$decimal_degree
