b7bb189bb9de5ca66782374feada14d10056bcca
Danielle Dempsey <ddempsey@perennia.ca>
2025-04-28 09:16:41 -0300

    generate simplified log from metadata

--- R/ss_create_old_log_from_metadata.R
+++ R/ss_create_old_log_from_metadata.R
@@ -30,7 +30,7 @@
 #'
 #' @export
 
-ss_create_log_from_metadata <- function(
+ss_create_old_log_from_metadata <- function(
     path_metadata = NULL,
     sheet = "tracker",
     path_export,
@@ -59,7 +59,7 @@ ss_create_log_from_metadata <- function(
     stop(paste0(station_title, " not found in metatdata tracking sheet"))
   }
 
-  dat <- dat_raw %>%
+ dat <- dat_raw %>%
     mutate(
       deployment_latitude = if_else(
         is.na(deployment_latitude),
@@ -89,21 +89,64 @@ ss_create_log_from_metadata <- function(
       station == !!station_title,
       deployment_date == !!as_date(deployment_date)
     ) %>%
-    select(
-      county, waterbody, station, lease,
+    transmute(
+      Deployment_Waterbody = waterbody,
+      Location_Description = station_title,
+      `Lease#` = lease,
+      Status = status,
 
-      deployment_date, retrieval_date,
+      Deployment = deployment_date,
+      Retrieval =  retrieval_date,
+      Duration = difftime(retrieval_date, deployment_date),
+      Logger_Latitude = deployment_latitude,
+      Logger_Longitude = deployment_longitude,
+      Logger_Model = sensor_type,
+      `Serial#` = sensor_serial_number,
+      Sensor_Depth = sensor_depth_m,
 
-      deployment_latitude, deployment_longitude,
-      retrieval_latitude, retrieval_longitude,
-      sensor_type, sensor_serial_number, sensor_depth_m,
+      Configuration = string_configuration,
+
+      Sounding = sounding_m,
+      Datum = NA,
+      Mount_type = NA,
+
+      #`Acoustic_Release?` = ifelse(Recv_Method == "Acoustic_Release", "Y", NA_character_),
+      #`Surface_Buoy?` = ifelse(Recv_Method == "Surface Buoy", "Y", NA_character_),
+
+      Deployment_Attendant = deployment_attendant,
+      Retrieval_Attendant = retrieval_attendant,
+
+      Comments = notes,
+
+      # `Deployment Waypoint` = NA,
+      #`Retrieval Waypoint` = NA,
+
+      `Retrieval Latitude`	= retrieval_latitude,
+      `Retrieval Longitude` =	retrieval_longitude,
+
+      #`Sensor Voltage deployed` = Depl_Voltage,
+      #`Sensor Voltage retrieved` = Recv_Voltage,
+
+      #`Vessel sounder offset + transponder depth` = NA,
+      #`verified measurement (below origin, first sensor under float)` = NA,
+      #`tide correction` = NA,
+      #`Rising or Falling` = NA,
+      #`height of VR2AR base off bottom` = NA,
+
+      #`time of deployment` = Depl_Time,
+      #`photos taken?` = NA,
+
+      #`Anchor type` = Anchor_Wgt,
+      #`Float type` = NA,
+      #`distance from top of float to origin (first sensor)` = NA
     ) %>%
     dplyr::mutate(
       #Location_Description = str_to_title(Location_Description),
-      sensor_serial_number = as.numeric(sensor_serial_number),
-      sensor_depth_m = as.numeric(sensor_depth_m),
-      deployment_date = format(as_date(deployment_date), "%Y-%m-%d"),
-      retrieval_date = format(as_date(retrieval_date), "%Y-%m-%d"),
+      `Serial#` = as.numeric(`Serial#`),
+      Sensor_Depth = as.numeric(Sensor_Depth),
+      Sounding = as.numeric(Sounding),
+      Deployment = format(as_date(Deployment), "%Y-%b-%d"),
+      Retrieval = format(as_date(Retrieval), "%Y-%b-%d"),
     )
 
   if (nrow(log) == 0) {
@@ -111,6 +154,10 @@ ss_create_log_from_metadata <- function(
       paste("No rows found in metatdata for << ", station, deployment_date, " >>")
     )
   }
+
+  # if(suppressWarnings(is.numeric(as.numeric(LOG$Sensor_Depth)))){
+  #   LOG <- LOG %>% mutate(Sensor_Depth = as.numeric(Sensor_Depth))
+  # }
 
   # Format & Export ---------------------------------------------------------
   file_name <- paste(
